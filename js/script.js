b72178f09f943e72afbf014484f1f986087833e8
kimnamlhn <kimnam.cpc@gmail.com>
2021-04-19 15:12:04 +0700

    a website to calc your height

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// hàm respond 
+function submitAnswer(){
+    var yourHeight = document.getElementById("your-answer").value;
+    // if(yourHeight == null)
+    //     alert("Hãy nhập vào chiều cao của bạn!!!");
+    var answer = "Chiều cao của bạn là: " + yourHeight + " cm";
+    alert(answer);
+}
